e940b84ac3218255f29983eb7ace735a8e235bb9
jecaudal <jecaudal@student.42.fr>
2020-09-01 12:29:34 +0200

    :arrow_up: feat ftps script simplification to use only two env var

--- srcs/ftps/srcs/init.sh
+++ srcs/ftps/srcs/init.sh
@@ -6,9 +6,13 @@
 #    By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/24 12:28:41 by jecaudal          #+#    #+#              #
-#    Updated: 2020/08/30 18:10:16 by jecaudal         ###   ########.fr        #
+#    Updated: 2020/09/01 12:24:43 by jecaudal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+# --- Env variables
+# FTPS_USER <- Username of the user you want to create
+# FTPS_PASS <- The password of the user
 
 # --- FTPS setup ---
 openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:1024 -keyout \
@@ -16,32 +20,21 @@ openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:1024 -keyou
 
 # --- Create FTP user ---
 # If there isn't users asked, it will create default user.
-if [ -z "$USERS" ]; then
-  USERS="anon|nona"
+if [ -z "$FTPS_USER" ]
+then
+	FTPS_USER="michel"
+	FTPS_PASS="gondri"
 fi
 
-for i in $USERS ; do
-    NAME=$(echo $i | cut -d'|' -f1)
-    PASS=$(echo $i | cut -d'|' -f2)
-  FOLDER=$(echo $i | cut -d'|' -f3)
-     UID=$(echo $i | cut -d'|' -f4)
+if [ ! -d "/ftp/$FTPS_USER" ]
+then
+	mkdir -p /ftp/$FTPS_USER
+fi
 
-  if [ -z "$FOLDER" ]; then
-    FOLDER="/ftp/$NAME"
-  fi
-
-  if [ ! -z "$UID" ]; then
-    UID_OPT="-u $UID"
-  fi
-
-# "/sbin/nologin" to create a non-logable user. It focus this user on ftp service.
-  echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /sbin/nologin $UID_OPT $NAME
-  mkdir -p $FOLDER
-  chown $NAME:$NAME $FOLDER
-  unset NAME PASS FOLDER UID
-done
+echo -e "$FTPS_PASS\n$FTPS_PASS" | adduser -h /ftp/$FTPS_USER $FTPS_USER
+chown $FTPS_USER:$FTPS_USER /ftp/$FTPS_USER
 
 # -- Start FTP server ---
 printf "FTPS server is starting !\n"
-exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21004 -opasv_address=$ADDRESS /etc/vsftpd/vsftpd.conf &
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21004 /etc/vsftpd/vsftpd.conf &
 tail -F /dev/null
