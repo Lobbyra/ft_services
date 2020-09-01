83430915049b8ce1068a75aff6c69d549283e756
jecaudal <jecaudal@student.42.fr>
2020-09-01 15:18:10 +0200

    :arrow_up: feat : add ssh to nginx with its root password in env

--- srcs/nginx/srcs/init.sh
+++ srcs/nginx/srcs/init.sh
@@ -6,9 +6,14 @@
 #    By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/22 17:40:08 by jecaudal          #+#    #+#              #
-#    Updated: 2020/08/30 16:06:12 by Lobbyra          ###   ########.fr        #
+#    Updated: 2020/09/01 15:02:33 by jecaudal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+# --- Env Variables
+# SSH_ROOT_PASSWORD <- Password to access to nginx container through SSH
+
+echo "root:$SSH_ROOT_PASSWORD" | chpasswd
 
 # Creating SSL keys
 openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt
@@ -18,9 +23,10 @@ adduser -D -g 'www' www
 chown -R www:www /var/lib/nginx
 chown -R www:www /www
 
-# Nginx starting
+# Nginx & SSH starting
 openrc
 touch /run/openrc/softlevel
-service nginx start 2> /dev/null
+service nginx start
+service sshd start
 
 tail -F /dev/null
