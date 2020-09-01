8dbb5965876d83c55e0029484a39abcfbda99740
jecaudal <jecaudal@student.42.fr>
2020-09-01 11:58:59 +0200

    :arrow_up: feat: create a specific user for the telegraf db.

--- srcs/influxdb/srcs/init.sh
+++ srcs/influxdb/srcs/init.sh
@@ -6,15 +6,13 @@
 #    By: jecaudal <jecaudal@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/25 18:06:13 by jecaudal          #+#    #+#              #
-#    Updated: 2020/08/26 15:16:29 by jecaudal         ###   ########.fr        #
+#    Updated: 2020/09/01 11:57:01 by jecaudal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # --- Env variables
 # INFLUX_ADM_PASS <- InfluxDB admin password.
 # INFLUX_TELE_PASS <- Telegram account password in influxDB.
-# DB_NAME <- Name of the database you want to complete with telegraf.
-#			/!\ The DB is created if needed.
 
 openrc
 touch /run/openrc/softlevel
@@ -22,7 +20,9 @@ service influxdb start
 sleep 2
 influx << EOF
 CREATE USER admin WITH PASSWORD '$INFLUX_ADM_PASS' WITH ALL PRIVILEGES;
-CREATE USER telegraf WITH PASSWORD '$INFLUX_TELE_PASS' WITH ALL PRIVILEGES;
+CREATE DATABASE telegraf;
+CREATE USER user_telegraf WITH PASSWORD '$INFLUX_TELE_PASS';
+GRANT ALL ON telegraf TO user_telegraf;
 EOF
 
 envsubst '${INFLUX_TELE_PASS}' < /root/telegraf.conf > /etc/telegraf/telegraf.conf
